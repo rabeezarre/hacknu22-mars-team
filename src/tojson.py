77fea4ef5b84c5985de24d7b93b8d40fa2fbd126
Sabina Jangirova <sabina.jangirova@nu.edu.kz>
2022-09-24 12:17:03 +0600

    json, labels

--- src/tojson.py
+++ src/tojson.py
@@ -4,22 +4,25 @@ from openpyxl.cell.read_only import EmptyCell
 
 workbook = openpyxl.load_workbook('hacknu-dev-data.xlsx')
 
+cases = []
 for sheet in workbook.worksheets:
-    with open('assets/cases/'+sheet.title+'.json', 'w') as file:
-        points = []
-        for row in range(2,sheet.max_row+1):
-            if sheet.cell(row=row, column=1).value is None:
-                break
-            points.append({
-                'Latitude':sheet.cell(row=row, column=1).value,
-                'Longitude':sheet.cell(row=row, column=2).value,
-                'Altitude':sheet.cell(row=row, column=3).value,
-                'Identifier':sheet.cell(row=row, column=4).value,
-                'Timestamp':sheet.cell(row=row, column=5).value,
-                'Floor label':sheet.cell(row=row, column=6).value,
-                'Horizontal accuracy':sheet.cell(row=row, column=7).value,
-                'Vertical accuracy':sheet.cell(row=row, column=8).value,
-                'Confidence in location accuracy':sheet.cell(row=row, column=9).value,
-                'Activity':sheet.cell(row=row, column=10).value,
-            })
-        file.write(json.dumps(points))
+    points = []
+    for row in range(2,sheet.max_row+1):
+        if sheet.cell(row=row, column=1).value is None:
+            break
+        points.append({
+            'Latitude':sheet.cell(row=row, column=1).value,
+            'Longitude':sheet.cell(row=row, column=2).value,
+            'Altitude':sheet.cell(row=row, column=3).value,
+            'Identifier':sheet.cell(row=row, column=4).value,
+            'Timestamp':sheet.cell(row=row, column=5).value,
+            'Floor label':sheet.cell(row=row, column=6).value,
+            'Horizontal accuracy':sheet.cell(row=row, column=7).value,
+            'Vertical accuracy':sheet.cell(row=row, column=8).value,
+            'Confidence in location accuracy':sheet.cell(row=row, column=9).value,
+            'Activity':sheet.cell(row=row, column=10).value,
+        })
+    cases.append(points)
+excel_json = open('assets/cases.json', 'w')
+excel_json.write(json.dumps(cases))
+excel_json.close()
